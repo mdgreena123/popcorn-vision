24462dc66c6d2a096699d4c9f31218cf2095441a
timothyrushing <timrushing001@gmail.com>
2023-07-05 20:19:33 +0700

    Update: Restore

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,70 +6,68 @@ import Copyright from "./components/Copyright";
 const keywords =
   "movies, TV shows, film reviews, movie database, TV series, film news, cinema releases, movie trailers, film recommendations, popular movies, top TV shows, movie ratings, celebrity news, upcoming releases, film analysis, binge-watching, movie genres, movie quotes, TV show episodes, box office hits, award-winning films";
 
-export async function generateMetadata() {
-  return {
-    generator: "Popcorn Vision",
-    applicationName: "Popcorn Vision",
-    referrer: "origin-when-cross-origin",
-    keywords: keywords.split(", "),
-    authors: [
-      { name: "Fachry Dwi Afriza", url: "https://fachryafrz.vercel.app" },
-    ],
-    colorScheme: "dark",
-    creator: "Fachry Dwi Afriza",
-    publisher: "Fachry Dwi Afriza",
+export const metadata = {
+  generator: "Popcorn Vision",
+  applicationName: "Popcorn Vision",
+  referrer: "origin-when-cross-origin",
+  keywords: keywords.split(", "),
+  authors: [
+    { name: "Fachry Dwi Afriza", url: "https://fachryafrz.vercel.app" },
+  ],
+  colorScheme: "dark",
+  creator: "Fachry Dwi Afriza",
+  publisher: "Fachry Dwi Afriza",
 
-    viewport: {
-      width: "device-width",
-      initialScale: 1,
-    },
-    title: {
-      default: "Popcorn Vision",
-      template: "%s - Popcorn Vision",
-    },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  title: {
+    default: "Popcorn Vision",
+    template: "%s - Popcorn Vision",
+  },
+  description:
+    "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
+  metadataBase: new URL("https://www.popcorn.vision"),
+  alternates: {
+    canonical: "/",
+    languages: "en-US",
+  },
+  openGraph: {
+    title: "Popcorn Vision",
     description:
       "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
-    metadataBase: new URL("https://www.popcorn.vision"),
-    alternates: {
-      canonical: "/",
-      languages: "en-US",
+    url: "https://www.popcorn.vision",
+    siteName: "Popcorn Vision",
+    images: "/popcorn.png",
+    locale: "en_US",
+    type: "website",
+  },
+  icons: {
+    icon: "/popcorn.png",
+    shortcut: "/popcorn.png",
+    apple: "/apple-touch-icon.png",
+  },
+  themeColor: "#202735",
+  manifest: "/manifest.json",
+  twitter: {
+    card: "summary_large_image",
+    title: "Popcorn Vision",
+    description:
+      "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
+    creator: "@fachryafrz",
+    images: "/popcorn.png",
+  },
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+    other: {
+      me: ["fachrydwiafriza@gmail.com", "https://fachryafrz.vercel.app"],
     },
-    openGraph: {
-      title: "Popcorn Vision",
-      description:
-        "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
-      url: "https://www.popcorn.vision",
-      siteName: "Popcorn Vision",
-      images: "/popcorn.png",
-      locale: "en_US",
-      type: "website",
-    },
-    icons: {
-      icon: "/popcorn.png",
-      shortcut: "/popcorn.png",
-      apple: "/apple-touch-icon.png",
-    },
-    themeColor: "#202735",
-    manifest: "/manifest.json",
-    twitter: {
-      card: "summary_large_image",
-      title: "Popcorn Vision",
-      description:
-        "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
-      creator: "@fachryafrz",
-      images: "/popcorn.png",
-    },
-    verification: {
-      google: "google",
-      yandex: "yandex",
-      yahoo: "yahoo",
-      other: {
-        me: ["fachrydwiafriza@gmail.com", "https://fachryafrz.vercel.app"],
-      },
-    },
-    category: "entertainment",
-  };
-}
+  },
+  category: "entertainment",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -77,6 +75,11 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/popcorn.png" type="image/png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <title>Popcorn Vision</title>
+        <meta
+          name="description"
+          content="Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision."
+        />
       </head>
       <body className="bg-base-dark-gray text-white">
         {/* Navbar */}
--- app/tv/page.jsx
+++ app/tv/page.jsx
@@ -4,39 +4,37 @@ import HomeSlider from "../components/HomeSlider";
 import FilmSlider from "../components/FilmSlider";
 import Trending from "../components/Trending";
 
-export async function generateMetadata() {
-  return {
-    title: "TV",
+export const metadata = {
+  title: "TV",
+  description:
+    "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
+  alternates: {
+    canonical: "https://www.popcorn.vision",
+  },
+  openGraph: {
+    title: "Popcorn Vision",
     description:
       "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
-    alternates: {
-      canonical: "https://www.popcorn.vision",
-    },
-    openGraph: {
-      title: "Popcorn Vision",
-      description:
-        "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
-      url: "https://www.popcorn.vision",
-      siteName: "Popcorn Vision",
-      images: "/popcorn.png",
-      locale: "en_US",
-      type: "website",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Popcorn Vision",
-      description:
-        "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
-      creator: "@fachryafrz",
-      images: "/popcorn.png",
-    },
-    icons: {
-      icon: "/popcorn.png",
-      shortcut: "/popcorn.png",
-      apple: "/apple-touch-icon.png",
-    },
-  };
-}
+    url: "https://www.popcorn.vision",
+    siteName: "Popcorn Vision",
+    images: "/popcorn.png",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Popcorn Vision",
+    description:
+      "Your go-to website for movies and TV shows info. Explore a wide range of titles, with detailed synopses, reviews, cast and crew info. Convenient search functionality makes finding your favorites a breeze. Dive into the world of movies and television with Popcorn Vision.",
+    creator: "@fachryafrz",
+    images: "/popcorn.png",
+  },
+  icons: {
+    icon: "/popcorn.png",
+    shortcut: "/popcorn.png",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 async function getGenres() {
   const res = await axios.get(`${process.env.API_URL}/genre/tv/list`, {

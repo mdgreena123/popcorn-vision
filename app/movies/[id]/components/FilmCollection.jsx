9e61ddcef0201e43abb824e0f255edade36a894f
timothyrushing <timrushing001@gmail.com>
2023-08-31 17:24:12 +0700

    Update: Limit data view

--- app/movies/[id]/components/FilmCollection.jsx
+++ app/movies/[id]/components/FilmCollection.jsx
@@ -8,6 +8,12 @@ export default function FilmCollection({ film }) {
   const [apiData, setApiData] = useState();
   const [collectionTitle, setCollectionTitle] = useState();
   const [collections, setCollections] = useState({});
+  const [showAllCollection, setShowAllCollection] = useState(false);
+  const numCollection = 3;
+
+  const handleShowAllCollection = () => {
+    setShowAllCollection(true);
+  };
 
   useEffect(() => {
     const fetchCollections = async () => {
@@ -36,6 +42,7 @@ export default function FilmCollection({ film }) {
     };
 
     fetchCollections();
+    setShowAllCollection(false);
   }, [film]);
 
   return (
@@ -45,62 +52,76 @@ export default function FilmCollection({ film }) {
           {apiData && collectionTitle}
         </h2>
       </div>
-      <ul className="flex flex-col gap-1">
+      <ul className="flex flex-col gap-1 relative">
         {apiData &&
-          collections.map((item, index) => {
-            return (
-              <li key={index}>
-                <Link
-                  href={`/movies/${item.id}`}
-                  className={`flex items-center gap-2 bg-base-gray bg-opacity-10 hocus:bg-opacity-30 p-2 rounded-xl w-full ${
-                    film.id === item.id && `bg-primary-blue bg-opacity-30`
-                  }`}
-                >
-                  <span className={`text-gray-400 text-sm font-medium px-1`}>
-                    {index + 1}
-                  </span>
+          collections
+            .slice(0, showAllCollection ? collections.length : numCollection)
+            .map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link
+                    href={`/movies/${item.id}`}
+                    className={`flex items-center gap-2 bg-base-gray bg-opacity-10 hocus:bg-opacity-30 p-2 rounded-xl w-full ${
+                      film.id === item.id && `bg-primary-blue bg-opacity-30`
+                    }`}
+                  >
+                    <span className={`text-gray-400 text-sm font-medium px-1`}>
+                      {index + 1}
+                    </span>
 
-                  <div className="aspect-poster min-w-[50px] max-w-[50px] rounded-lg overflow-hidden flex items-center">
-                    {item.poster_path ? (
-                      <figure className={`w-full`}>
-                        <img
-                          src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                          alt={item.title}
-                          className={`object-contain`}
-                        />
-                      </figure>
-                    ) : (
-                      <figure
-                        style={{
-                          background: `url(/popcorn.png)`,
-                          backgroundSize: `contain`,
-                        }}
-                        className={`aspect-square w-[50px]`}
-                      ></figure>
-                    )}
-                  </div>
-                  <div className="flex flex-col gap-1 items-start w-full">
-                    <h3
-                      className="text-start line-clamp-2 font-medium"
-                      title={item.title}
-                    >
-                      {item.title}
-                    </h3>
-
-                    <div className="text-sm text-gray-400 font-medium">
-                      {item.release_date
-                        ? new Date(item.release_date).getFullYear()
-                        : `Coming soon`}
+                    <div className="aspect-poster min-w-[50px] max-w-[50px] rounded-lg overflow-hidden flex items-center">
+                      {item.poster_path ? (
+                        <figure className={`w-full`}>
+                          <img
+                            src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                            alt={item.title}
+                            className={`object-contain`}
+                          />
+                        </figure>
+                      ) : (
+                        <figure
+                          style={{
+                            background: `url(/popcorn.png)`,
+                            backgroundSize: `contain`,
+                          }}
+                          className={`aspect-square w-[50px]`}
+                        ></figure>
+                      )}
                     </div>
-                  </div>
+                    <div className="flex flex-col gap-1 items-start w-full">
+                      <h3
+                        className="text-start line-clamp-2 font-medium"
+                        title={item.title}
+                      >
+                        {item.title}
+                      </h3>
 
-                  <p className="text-xs text-gray-400 line-clamp-3 w-full">
-                    {item.overview}
-                  </p>
-                </Link>
-              </li>
-            );
-          })}
+                      <div className="text-sm text-gray-400 font-medium">
+                        {item.release_date
+                          ? new Date(item.release_date).getFullYear()
+                          : `Coming soon`}
+                      </div>
+                    </div>
+
+                    <p className="text-xs text-gray-400 line-clamp-3 w-full">
+                      {item.overview}
+                    </p>
+                  </Link>
+                </li>
+              );
+            })}
+
+        {apiData && collections.length > numCollection && (
+          <div
+            className={`absolute inset-x-0 bottom-0 bg-gradient-to-t from-base-dark-gray justify-center items-end h-[200px] text-primary-blue ${
+              showAllCollection ? `hidden` : `flex`
+            }`}
+          >
+            <button onClick={handleShowAllCollection}>
+              View all collection
+            </button>
+          </div>
+        )}
       </ul>
     </div>
   );
--- app/movies/[id]/components/FilmReviews.jsx
+++ app/movies/[id]/components/FilmReviews.jsx
@@ -6,6 +6,12 @@ export default function FilmReviews({ reviews, film }) {
   const totalReviewPages = reviews.total_pages;
   let [currentReviewPage, setCurrentReviewPage] = useState(1);
   const [moreReviews, setMoreReviews] = useState(reviews.results);
+  const [showAllReviews, setShowAllReviews] = useState(false);
+  const numReviews = 3;
+
+  const handleShowAllReviews = () => {
+    setShowAllReviews(true);
+  };
 
   const fetchMoreReviews = async () => {
     setCurrentReviewPage((prevPage) => prevPage + 1);
@@ -30,25 +36,42 @@ export default function FilmReviews({ reviews, film }) {
 
   useEffect(() => {
     setCurrentReviewPage(1);
+    setShowAllReviews(false);
   }, [film]);
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 relative">
       <div className="flex gap-4 items-center justify-between bg-base-dark-gray backdrop-blur bg-opacity-[85%] sticky top-[4.125rem] py-2 z-10">
         <h2 className="font-bold text-xl text-white m-0">Reviews</h2>
       </div>
       <div className="flex flex-col gap-2">
-        {moreReviews.map((review, index) => {
-          return <ReviewCard key={index} review={review} />;
-        })}
+        {moreReviews
+          .slice(0, showAllReviews ? moreReviews.length : numReviews)
+          .map((review, index) => {
+            return <ReviewCard key={index} review={review} />;
+          })}
       </div>
-      {totalReviewPages > 1 && currentReviewPage !== totalReviewPages && (
-        <button
-          onClick={() => fetchMoreReviews((currentReviewPage += 1))}
-          className="text-primary-blue py-2 flex justify-center hocus:bg-white hocus:bg-opacity-10 rounded-lg"
+
+      {totalReviewPages > 1 &&
+        showAllReviews &&
+        currentReviewPage !== totalReviewPages && (
+          <button
+            onClick={() => fetchMoreReviews((currentReviewPage += 1))}
+            className="text-primary-blue py-2 flex justify-center hocus:bg-white hocus:bg-opacity-10 rounded-lg"
+          >
+            Load more reviews
+          </button>
+        )}
+
+      {/* View all reviews */}
+      {moreReviews.length > numReviews && (
+        <div
+          className={`absolute inset-x-0 bottom-0 bg-gradient-to-t from-base-dark-gray justify-center items-end h-[200px] text-primary-blue ${
+            showAllReviews ? `hidden` : `flex`
+          }`}
         >
-          Load more reviews
-        </button>
+          <button onClick={handleShowAllReviews}>View all reviews</button>
+        </div>
       )}
     </div>
   );

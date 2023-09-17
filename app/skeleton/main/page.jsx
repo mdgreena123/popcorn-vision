7334b39f73fccba98f8dea54501242f5db074bab
timothyrushing <timrushing001@gmail.com>
2023-09-17 20:45:11 +0700

    Update: Skeleton

--- app/skeleton/main/page.jsx
+++ app/skeleton/main/page.jsx
@@ -1,5 +1,10 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 export default function MainLoading() {
-  const sectionCount = 2;
+  const sectionCount = 3;
   const itemCount = 7;
 
   return (
@@ -38,31 +43,89 @@ export default function MainLoading() {
       {[...Array(sectionCount).keys()].map((a) => (
         <section
           key={a}
-          className={`max-w-full xl:max-w-7xl mx-auto flex flex-col gap-4 [&_*]:rounded-lg !bg-opacity-0 p-4`}
+          className={`w-full max-w-full xl:max-w-7xl mx-auto flex flex-col gap-4 [&_*]:rounded-lg !bg-opacity-0 p-4`}
         >
           {/* Section Title */}
           <div className={`h-[28px] w-[100px]`}></div>
-          <div
-            className={`flex gap-2 flex-nowrap overflow-hidden !bg-opacity-0`}
+
+          {/* Films */}
+          <Swiper
+            spaceBetween={8}
+            slidesPerView={2}
+            breakpoints={{
+              640: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+              },
+              768: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+              },
+              1024: {
+                slidesPerView: 5,
+                slidesPerGroup: 5,
+              },
+            }}
+            allowSlideNext={false}
+            allowSlidePrev={false}
+            allowTouchMove={false}
+            className={`!bg-opacity-0 w-full !pr-[3rem]`}
+            wrapperClass={`!bg-opacity-0`}
           >
             {[...Array(itemCount).keys()].map((b) => (
-              <div key={b} className={`flex flex-col gap-1 !bg-opacity-0`}>
+              <SwiperSlide key={b} className={`!bg-opacity-0`}>
                 {/* Poster */}
-                <div
-                  className={`h-[250px] w-[40vw] sm:w-[28vw] md:w-[23.5vw] lg:w-[19vw] xl:w-[13vw]`}
-                ></div>
-                {/* Film Title */}
-                <div className={`h-[20px]`}></div>
+                <div className={`aspect-poster`}></div>
+
+                {/* Title */}
+                <div className={`h-[28px] mt-2`}></div>
+
                 {/* Release Date & Genres */}
-                <div className={`h-[1rem] !bg-opacity-0 flex gap-1`}>
-                  <div className={`h-full w-[30%]`}></div>
-                  <div className={`h-full w-[70%]`}></div>
+                <div className={`h-[20px] mt-1 flex gap-1 !bg-opacity-0`}>
+                  <div className={`w-[30%]`}></div>
+                  <div className={`w-[70%]`}></div>
                 </div>
-              </div>
-            ))}{" "}
-          </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </section>
       ))}
+
+      {/* Trending */}
+      <section className={`px-4 !bg-opacity-0 mx-auto max-w-7xl w-full`}>
+        <div
+          className={`relative rounded-[3rem] p-[3rem] w-full overflow-hidden flex items-center gap-8 before:absolute before:inset-0 before:bg-gradient-to-r before:from-black before:via-[40%] !bg-opacity-5`}
+        >
+          {/* Background */}
+          <div className={`absolute inset-0 -z-10 blur-md md:blur-0`}></div>
+          {/* Poster */}
+          <div className={`aspect-poster min-w-[300px] rounded-2xl`}></div>
+
+          {/* Details */}
+          <div
+            className={`flex-1 !bg-opacity-0 flex flex-col justify-end items-center md:items-start gap-2 p-4 [&_*]:rounded-lg mb-4`}
+          >
+            {/* Title Logo */}
+            <div className={`h-[100px] w-full !max-w-[300px]`}></div>
+            {/* Rating, Release Date, Season, Genre */}
+            <div
+              className={`flex items-center justify-center md:justify-start gap-2 !bg-opacity-0 w-full [&_*]:w-full [&_*]:!max-w-[75px]`}
+            >
+              <div className={`h-[24px] w-full`}></div>
+              <div className={`h-[24px] w-full`}></div>
+              <div className={`h-[24px] w-full`}></div>
+            </div>
+            {/* Overview */}
+            <div
+              className={`h-[100px] w-full !bg-opacity-0 flex flex-col gap-2 items-center md:items-start [&_*]:!max-w-none`}
+            >
+              <div className={`h-[24px] w-full`}></div>
+              <div className={`h-[24px] w-full`}></div>
+              <div className={`h-[24px] w-[80%]`}></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

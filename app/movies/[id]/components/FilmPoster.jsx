b8a4e04d901cc65218b998f197742624e4264648
timothyrushing <timrushing001@gmail.com>
2023-11-29 10:34:58 +0700

    Update 29 Nov 2023: add background for empty film poster

--- app/movies/[id]/components/FilmPoster.jsx
+++ app/movies/[id]/components/FilmPoster.jsx
@@ -16,28 +16,27 @@ export default function FilmPoster({ film }) {
   };
 
   let popcorn = `/popcorn.png`;
-  let filmPoster = `https://image.tmdb.org/t/p/w500${film.poster_path}`;
+  let filmPoster = film.poster_path;
 
   return (
     <div className="sticky top-20 flex flex-col gap-4 h-fit w-full">
       <figure
-        className={`aspect-poster rounded-xl overflow-hidden self-start shadow-xl relative w-full`}
+        className={`aspect-poster rounded-xl overflow-hidden self-start shadow-xl relative w-full bg-base-100`}
         style={{
-          backgroundImage: film.poster_path !== null ? filmPoster : `url(${popcorn})`,
-          backgroundSize: film.poster_path === null ? `contain` : `cover` ,
+          backgroundImage:
+            film.poster_path === null
+              ? `url(${popcorn})`
+              : `url(https://image.tmdb.org/t/p/w45${filmPoster})`,
+          backgroundSize: film.poster_path === null ? `contain` : `cover`,
           backgroundRepeat: `no-repeat`,
           backgroundPosition: `center`,
         }}
       >
         {film.poster_path !== null && (
           <img
-            src={filmPoster}
+            src={`https://image.tmdb.org/t/p/w500${filmPoster}`}
             alt={isTvPage(film.title, film.name)}
-            className={`pointer-events-none ${
-              film.poster_path === null
-                ? `object-contain bg-base-100`
-                : `object-cover`
-            }`}
+            className={`pointer-events-none object-cover`}
           />
         )}
 

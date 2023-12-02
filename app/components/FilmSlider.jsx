de6bf853ecf4164375ec58a8630dfdde27f45157
timothyrushing <timrushing001@gmail.com>
2023-12-02 22:27:51 +0700

    Update 2 Des 2023: Fix slider

--- app/components/FilmSlider.jsx
+++ app/components/FilmSlider.jsx
@@ -29,34 +29,34 @@ export default function FilmSlider({ films, title, genres, sort = "DESC" }) {
   });
 
   return (
-    <section id={title}>
+    <section id={title} className={`px-4 w-full max-w-7xl mx-auto`}>
       <h2 className="sr-only">{title}</h2>
 
       <Swiper
         modules={[Navigation]}
         spaceBetween={8}
-        slidesPerView={2}
+        slidesPerView={`auto`}
         // loop={true}
         navigation={{
           nextEl: ".next",
           prevEl: ".prev",
           clickable: true,
         }}
-        // breakpoints={{
-        //   640: {
-        //     slidesPerView: 3,
-        //     slidesPerGroup: 3,
-        //   },
-        //   768: {
-        //     slidesPerView: 4,
-        //     slidesPerGroup: 4,
-        //   },
-        //   1024: {
-        //     slidesPerView: 5,
-        //     slidesPerGroup: 5,
-        //   },
-        // }}
-        className={`!px-4 !pb-[2rem] !pt-[2.5rem] max-w-7xl relative before:absolute before:inset-0 before:bg-gradient-to-r before:from-base-100 before:max-w-[8.35rem] before:z-10 after:absolute after:top-0 after:right-0 after:!w-[8.35rem] after:!h-full after:bg-gradient-to-l after:from-base-100 after:z-10 before:hidden after:hidden xl:before:hidden xl:after:block before:pointer-events-none after:pointer-events-none`}
+        breakpoints={{
+          640: {
+            // slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+          768: {
+            // slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          1024: {
+            // slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+        }}
+        className={`!pb-[2rem] !pt-[2.5rem] relative before:absolute before:inset-0 before:bg-gradient-to-r before:from-base-100 before:max-w-[8.35rem] before:z-10 after:absolute after:top-0 after:right-0 after:!w-[5rem] after:!h-full after:bg-gradient-to-l after:from-base-100 after:z-10 before:hidden after:hidden xl:before:hidden xl:after:block before:pointer-events-none after:pointer-events-none`}
       >
         {sortedFilms.map((film) => {
           const filmGenres =
@@ -69,7 +69,7 @@ export default function FilmSlider({ films, title, genres, sort = "DESC" }) {
           return (
             <SwiperSlide
               key={film.id}
-              className={`overflow-hidden hocus:scale-[1.025] active:scale-100 transition-all max-w-[calc(100%/2.5)] sm:max-w-[calc(100%/3.5)] md:max-w-[calc(100%/4.5)] lg:max-w-[calc(100%/5.5)] mr-2`}
+              className={`overflow-hidden hocus:scale-[1.025] active:scale-100 transition-all max-w-[calc(100%/2.5)] sm:max-w-[calc(100%/3.5)] md:max-w-[calc(100%/4.5)] lg:max-w-[calc(100%/5.5)]`}
             >
               <article>
                 <FilmCard film={film} genres={filmGenres} isTvPage={isTvPage} />
@@ -78,7 +78,7 @@ export default function FilmSlider({ films, title, genres, sort = "DESC" }) {
           );
         })}
 
-        <div className="z-20 absolute top-0 left-0 right-0 h-[28px] px-4 max-w-7xl xl:max-w-none flex justify-between items-end">
+        <div className="z-20 absolute top-0 left-0 right-0 h-[28px] max-w-7xl xl:max-w-none flex justify-between items-end">
           <div className="flex gap-4 items-end">
             <p className="font-bold text-lg md:text-2xl">{title}</p>
           </div>

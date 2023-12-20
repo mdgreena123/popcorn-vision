96fcc7648e92657deb4cdb8dbe3a7e0286700bd2
timothyrushing <timrushing001@gmail.com>
2023-12-20 19:07:13 +0700

    add: Reviews count

--- app/movies/[id]/components/FilmReviews.jsx
+++ app/movies/[id]/components/FilmReviews.jsx
@@ -25,10 +25,11 @@ export default function FilmReviews({ reviews, film }) {
 
   return (
     <div className="flex flex-col gap-2 relative">
-      <div className="flex gap-4 items-center justify-between bg-base-100 backdrop-blur bg-opacity-[85%] sticky top-[66px] py-2 z-10">
+      <div className="flex gap-1 items-center bg-base-100 backdrop-blur bg-opacity-[85%] sticky top-[66px] py-2 z-10">
         <h2 className="font-bold text-xl text-white m-0">
           {moreReviews.length > 1 ? `Reviews` : `Review`}
         </h2>
+        <span className={`text-sm text-gray-400`}>({moreReviews.length})</span>
       </div>
       <div className="flex flex-col gap-2">
         {moreReviews

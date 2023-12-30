9ed290bb02d1e4d540c7b2fb42a54daf156704ad
timothyrushing <timrushing001@gmail.com>
2023-12-30 12:13:30 +0700

    fix: null episode runtime to 0

--- app/movies/[id]/components/EpisodeModal.jsx
+++ app/movies/[id]/components/EpisodeModal.jsx
@@ -70,7 +70,9 @@ export function EpisodeModal({
               />
             )}
           </figure>
-          <div className={`p-4 sm:p-8 -mt-[75px] z-10 relative flex flex-col gap-6`}>
+          <div
+            className={`p-4 sm:p-8 -mt-[75px] z-10 relative flex flex-col gap-6`}
+          >
             <h1
               title={episode.name}
               className={`text-3xl sm:text-4xl text-center font-bold`}
@@ -116,7 +118,7 @@ export function EpisodeModal({
                 <div className={`flex items-center gap-2`}>
                   <IonIcon icon={timeOutline} />
                   <time>
-                    {episode.runtime}{" "}
+                    {episode.runtime ? episode.runtime : 0}{" "}
                     {isPlural({ text: "minute", number: episode.runtime % 60 })}
                   </time>
                   {Math.floor(episode.runtime / 60) >= 1 && (

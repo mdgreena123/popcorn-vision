3c9e3ded61bdc47c81917c25f41a1b0fd8d55c82
timothyrushing <timrushing001@gmail.com>
2024-08-06 16:30:31 +0700

    update: store user location to local storage

--- src/lib/navigator.js
+++ src/lib/navigator.js
@@ -1,31 +1,35 @@
 import { getLocation } from "./fetch";
 
 export function askLocation(setUserLocation, setError) {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const userLocation = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        };
+  if (localStorage.getItem("user-location")) {
+    setUserLocation(localStorage.getItem("user-location"));
+  } else {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLocation = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
 
-        const { latitude, longitude } = userLocation;
+          const { latitude, longitude } = userLocation;
 
-        // Asumsikan getLocation adalah fungsi yang mengembalikan lokasi berdasarkan koordinat
-        getLocation({ latitude, longitude }).then((response) => {
-          // localStorage.setItem("user-location", JSON.stringify(response));
-          setUserLocation(JSON.stringify(response));
-          setError(null); // Clear any previous errors
-        });
-      },
-      (error) => {
-        console.log(error);
-        if (error.code === error.PERMISSION_DENIED) {
-          setError(
-            "Location access denied by user. Please enable location access in your browser settings and try again.",
-          );
-        }
-      },
-    );
+          // Asumsikan getLocation adalah fungsi yang mengembalikan lokasi berdasarkan koordinat
+          getLocation({ latitude, longitude }).then((response) => {
+            localStorage.setItem("user-location", JSON.stringify(response));
+            setUserLocation(JSON.stringify(response));
+            setError(null); // Clear any previous errors
+          });
+        },
+        (error) => {
+          console.log(error);
+          if (error.code === error.PERMISSION_DENIED) {
+            setError(
+              "Location access denied by user. Please enable location access in your browser settings and try again.",
+            );
+          }
+        },
+      );
+    }
   }
 }

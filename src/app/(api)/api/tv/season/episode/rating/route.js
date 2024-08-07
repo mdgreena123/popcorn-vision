0b6fa6c0238af6a9d6ee7955cc23b22096cd3f8b
timothyrushing <timrushing001@gmail.com>
2024-08-07 20:58:45 +0700

    feat: rate episode

---
+++ src/app/(api)/api/tv/season/episode/rating/route.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const revalidate = true;
+
+export async function POST(req) {
+  const { id, season_number, episode_number, rating } = await req.json();
+  const cookiesStore = cookies();
+
+  try {
+    const { data } = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/tv/${id}/season/${season_number}/episode/${episode_number}/rating`,
+      {
+        value: rating,
+      },
+      {
+        params: {
+          api_key: process.env.API_KEY,
+          session_id: cookiesStore.get("tmdb.session_id").value,
+        },
+      },
+    );
+  } catch (error) {
+    return NextResponse.json(error.response.data, {
+      status: error.response.status,
+    });
+  } finally {
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/tv/${id}/season/${season_number}/episode/${episode_number}/account_states`,
+      {
+        params: {
+          api_key: process.env.API_KEY,
+          session_id: cookiesStore.get("tmdb.session_id").value,
+        },
+      },
+    );
+
+    return NextResponse.json(data, { status: 200 });
+  }
+}
+
+export async function DELETE(req) {
+  const url = new URL(req.url);
+  const { id, season_number, episode_number, rating } = Object.fromEntries(url.searchParams);
+  const cookiesStore = cookies();
+
+  try {
+    const { data } = await axios.delete(
+      `${process.env.NEXT_PUBLIC_API_URL}/tv/${id}/season/${season_number}/episode/${episode_number}/rating`,
+      {
+        params: {
+          api_key: process.env.API_KEY,
+          session_id: cookiesStore.get("tmdb.session_id").value,
+        },
+      },
+    );
+  } catch (error) {
+    return NextResponse.json(error.response.data, {
+      status: error.response.status,
+    });
+  } finally {
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/tv/${id}/season/${season_number}/episode/${episode_number}/account_states`,
+      {
+        params: {
+          api_key: process.env.API_KEY,
+          session_id: cookiesStore.get("tmdb.session_id").value,
+        },
+      },
+    );
+
+    return NextResponse.json(data, { status: 200 });
+  }
+}

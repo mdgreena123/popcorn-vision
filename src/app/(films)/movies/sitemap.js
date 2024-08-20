375f8b13c44794044d34c18a1f6f3af6aaefd5f6
timothyrushing <timrushing001@gmail.com>
2024-08-20 10:49:58 +0700

    remove: dynamic sitemap

--- src/app/(films)/movies/sitemap.js
+++ src/app/(films)/movies/sitemap.js
@@ -1,49 +1,49 @@
-import { fetchData } from "@/lib/fetch";
-import { slugify } from "@/lib/slugify";
+// import { fetchData } from "@/lib/fetch";
+// import { slugify } from "@/lib/slugify";
 
-export async function generateSitemaps() {
-  const sitemap = [];
+// export async function generateSitemaps() {
+//   const sitemap = [];
 
-  const { total_pages: totalPages } = await fetchData({
-    endpoint: `/discover/movie`,
-    queryParams: {
-      page: 1,
-    },
-  });
+//   const { total_pages: totalPages } = await fetchData({
+//     endpoint: `/discover/movie`,
+//     queryParams: {
+//       page: 1,
+//     },
+//   });
 
-  for (let i = 1; i <= 10; i++) {
-    sitemap.push({
-      id: i,
-    });
-  }
+//   for (let i = 1; i <= 10; i++) {
+//     sitemap.push({
+//       id: i,
+//     });
+//   }
 
-  return sitemap;
-}
+//   return sitemap;
+// }
 
-export default async function sitemap({ id }) {
-  const appUrl = process.env.NEXT_PUBLIC_APP_URL;
-  const page = id;
-  const sitemap = [];
+// export default async function sitemap({ id }) {
+//   const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+//   const page = id;
+//   const sitemap = [];
 
-  const movies = [];
-  for (let i = page; i <= page; i++) {
-    const { results } = await fetchData({
-      endpoint: `/discover/movie`,
-      queryParams: {
-        page: i,
-      },
-    });
-    movies.push(...results);
-  }
+//   const movies = [];
+//   for (let i = page; i <= page; i++) {
+//     const { results } = await fetchData({
+//       endpoint: `/discover/movie`,
+//       queryParams: {
+//         page: i,
+//       },
+//     });
+//     movies.push(...results);
+//   }
 
-  const moviesInSitemap = movies.map((movie) => ({
-    url: `${appUrl}/movies/${movie.id}-${slugify(movie.title)}`,
-    lastModified: new Date(),
-    changeFrequency: "hourly",
-    priority: 0.8,
-  }));
+//   const moviesInSitemap = movies.map((movie) => ({
+//     url: `${appUrl}/movies/${movie.id}-${slugify(movie.title)}`,
+//     lastModified: new Date(),
+//     changeFrequency: "hourly",
+//     priority: 0.8,
+//   }));
 
-  sitemap.push(...moviesInSitemap);
+//   sitemap.push(...moviesInSitemap);
 
-  // return sitemap;
-}
+//   return sitemap;
+// }
--- src/app/(films)/tv/sitemap.js
+++ src/app/(films)/tv/sitemap.js
@@ -1,49 +1,49 @@
-import { fetchData } from "@/lib/fetch";
-import { slugify } from "@/lib/slugify";
+// import { fetchData } from "@/lib/fetch";
+// import { slugify } from "@/lib/slugify";
 
-export async function generateSitemaps() {
-  const sitemap = [];
+// export async function generateSitemaps() {
+//   const sitemap = [];
 
-  const { total_pages: totalPages } = await fetchData({
-    endpoint: `/discover/tv`,
-    queryParams: {
-      page: 1,
-    },
-  });
+//   const { total_pages: totalPages } = await fetchData({
+//     endpoint: `/discover/tv`,
+//     queryParams: {
+//       page: 1,
+//     },
+//   });
 
-  for (let i = 1; i <= 10; i++) {
-    sitemap.push({
-      id: i,
-    });
-  }
+//   for (let i = 1; i <= 10; i++) {
+//     sitemap.push({
+//       id: i,
+//     });
+//   }
 
-  return sitemap;
-}
+//   return sitemap;
+// }
 
-export default async function sitemap({ id }) {
-  const appUrl = process.env.NEXT_PUBLIC_APP_URL;
-  const page = id;
-  const sitemap = [];
+// export default async function sitemap({ id }) {
+//   const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+//   const page = id;
+//   const sitemap = [];
 
-  const tvShows = [];
-  for (let i = page; i <= page; i++) {
-    const { results } = await fetchData({
-      endpoint: `/discover/tv`,
-      queryParams: {
-        page: i,
-      },
-    });
-    tvShows.push(...results);
-  }
+//   const tvShows = [];
+//   for (let i = page; i <= page; i++) {
+//     const { results } = await fetchData({
+//       endpoint: `/discover/tv`,
+//       queryParams: {
+//         page: i,
+//       },
+//     });
+//     tvShows.push(...results);
+//   }
 
-  const tvShowsInSitemap = tvShows.map((show) => ({
-    url: `${appUrl}/tv/${show.id}-${slugify(show.name)}`,
-    lastModified: new Date(),
-    changeFrequency: "hourly",
-    priority: 0.8,
-  }));
+//   const tvShowsInSitemap = tvShows.map((show) => ({
+//     url: `${appUrl}/tv/${show.id}-${slugify(show.name)}`,
+//     lastModified: new Date(),
+//     changeFrequency: "hourly",
+//     priority: 0.8,
+//   }));
 
-  sitemap.push(...tvShowsInSitemap);
+//   sitemap.push(...tvShowsInSitemap);
 
-  // return sitemap;
-}
+//   return sitemap;
+// }

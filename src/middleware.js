1430b015295f40fb2418ff3076f7cbef91e9f92a
timothyrushing <timrushing001@gmail.com>
2024-09-06 20:27:04 +0700

    update: add middleware for /login & /profile

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
 import { fetchData } from "./lib/fetch";
 import { slugify } from "./lib/slugify";
+import { cookies } from "next/headers";
 
 // Example of default export
 export default async function middleware(request) {
+  const cookiesStore = cookies();
+
   const pathname = request.nextUrl.pathname;
   const id = pathname.split("-")[0].split("/").pop();
   const isTvPage = pathname.startsWith("/tv");
@@ -23,8 +26,27 @@ export default async function middleware(request) {
       return NextResponse.redirect(new URL(correctPathname, request.url));
     }
   }
+
+  const isLoginPage = pathname.startsWith("/login");
+  const isProfilePage = pathname.startsWith("/profile");
+
+  if (isProfilePage) {
+    if (!cookiesStore.has("tmdb.session_id")) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  if (isLoginPage) {
+    const { searchParams } = request.nextUrl;
+
+    if (cookiesStore.has("tmdb.session_id")) {
+      return NextResponse.redirect(
+        new URL(searchParams.get("redirect_to") || "/", request.url),
+      );
+    }
+  }
 }
 
 export const config = {
-  matcher: ["/movies/:id/:path*", "/tv/:id/:path*"],
+  matcher: ["/movies/:id/:path*", "/tv/:id/:path*", "/profile", "/login"],
 };

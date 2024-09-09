93b767d408c7ff5a89dc580be88324c83eda2b2d
timothyrushing <timrushing001@gmail.com>
2024-09-09 22:22:39 +0700

    fix: securing API route

--- src/app/(api)/api/authentication/token/new/route.js
+++ src/app/(api)/api/authentication/token/new/route.js
@@ -2,10 +2,13 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
+  const url = new URL(request.url);
+  const { api_key } = Object.fromEntries(url.searchParams);
+  
   try {
     const { data, status } = await axios.get(
       `${process.env.NEXT_PUBLIC_API_URL}/authentication/token/new`,
-      { params: { api_key: process.env.API_KEY } },
+      { params: { api_key: api_key } },
     );
 
     return NextResponse.json(data, { status });

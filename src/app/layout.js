9329225ade9fd3573186ecc744faf77e8da0bee6
timothyrushing <timrushing001@gmail.com>
2024-09-13 23:45:12 +0700

    hide: robots & sitemap

--- src/app/layout.js
+++ src/app/layout.js
@@ -66,19 +66,19 @@ export const metadata = {
     },
   },
   category: "entertainment",
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: false,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
+  // robots: {
+  //   index: true,
+  //   follow: true,
+  //   nocache: false,
+  //   googleBot: {
+  //     index: true,
+  //     follow: true,
+  //     noimageindex: false,
+  //     "max-video-preview": -1,
+  //     "max-image-preview": "large",
+  //     "max-snippet": -1,
+  //   },
+  // },
 };
 
 export default function RootLayout({ children }) {

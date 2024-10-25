41f53135d794bdf7179a25eb87e4546a888ca42b
timothyrushing <timrushing001@gmail.com>
2024-10-26 01:19:13 +0700

    refactor: image optimization

--- src/components/Film/Details/Backdrop.jsx
+++ src/components/Film/Details/Backdrop.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 /* eslint-disable @next/next/no-img-element */
 import ImagePovi from "@/components/Film/ImagePovi";
@@ -12,11 +12,23 @@ export default function FilmBackdrop({ film }) {
         <ImagePovi
           imgPath={
             film.backdrop_path &&
-            `https://image.tmdb.org/t/p/original${film.backdrop_path}`
+            `https://image.tmdb.org/t/p/w500${film.backdrop_path}`
           }
           position={`top`}
           className={`absolute inset-0 z-0 aspect-video max-h-[100dvh] w-full overflow-hidden before:absolute before:inset-0 before:z-0 before:bg-gradient-to-t before:from-base-100 md:min-h-[500px] md:opacity-[50%] lg:max-h-[120dvh]`}
-        />
+        >
+          {film.backdrop_path && (
+            <figure
+              className={`h-full bg-base-100`}
+              style={{
+                backgroundImage: `url(https://image.tmdb.org/t/p/original${film.backdrop_path})`,
+                backgroundSize: "cover",
+                backgroundPosition: "top",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></figure>
+          )}
+        </ImagePovi>
       </Reveal>
     </>
   );

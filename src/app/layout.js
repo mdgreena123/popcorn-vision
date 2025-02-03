f191c0b30efdf0b1b5aa4fadbd31e3da69152eae
timothyrushing <timrushing001@gmail.com>
2025-02-03 12:48:47 +0700

    feat: update robots meta tag to prevent indexing by search engines

--- src/app/layout.js
+++ src/app/layout.js
@@ -74,19 +74,19 @@ export const metadata = {
     },
   },
   category: "entertainment",
-  // robots: {
-  //   index: true,
-  //   follow: true,
-  //   nocache: false,
-  //   googleBot: {
-  //     index: true,
-  //     follow: true,
-  //     noimageindex: false,
-  //     "max-video-preview": -1,
-  //     "max-image-preview": "large",
-  //     "max-snippet": -1,
-  //   },
-  // },
+  robots: {
+    index: false,
+    follow: false,
+    nocache: false,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default async function RootLayout({ children }) {

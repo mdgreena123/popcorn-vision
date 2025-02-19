9f484fa0399f0fbbbf20c52cb8b2635d90c3b93e
timothyrushing <timrushing001@gmail.com>
2025-02-19 15:27:01 +0700

    fix: increase stale time for dynamic and static content to 1 hour

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
   reactStrictMode: false,
   experimental: {
     staleTimes: {
-      dynamic: 1800, // 30 minutes in seconds
-      static: 1800,
+      dynamic: 3600,
+      static: 3600,
     },
   },
 };

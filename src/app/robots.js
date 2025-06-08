8a5ce3545d90dc193c87908ba367fa9a92b537d9
timothyrushing <timrushing001@gmail.com>
2025-06-09 00:58:27 +0700

    fix: update robots rules structure for better clarity

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,8 +1,26 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        disallow: "/",
+      },
+      {
+        userAgent: [
+          "Twitterbot",
+          "facebookexternalhit",
+          "facebot",
+          "Slackbot",
+          "Discordbot",
+          "WhatsApp",
+          "WhatsAppBot",
+          "vercel-og",
+          "vercel-favicon",
+          "vercel-screenshot",
+          "LinkedInBot",
+        ],
+        disallow: [" "],
+      },
+    ],
   };
 }

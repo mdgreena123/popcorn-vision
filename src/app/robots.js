d5d19e71cc72423ba49e3f98b4278981fc881598
timothyrushing <timrushing001@gmail.com>
2025-06-30 01:47:58 +0700

    chore: allow all user agent

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,26 +1,8 @@
 export default function robots() {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        disallow: "/",
-      },
-      {
-        userAgent: [
-          "Twitterbot",
-          "facebookexternalhit",
-          "facebot",
-          "Slackbot",
-          "Discordbot",
-          "WhatsApp",
-          "WhatsAppBot",
-          "vercel-og",
-          "vercel-favicon",
-          "vercel-screenshot",
-          "LinkedInBot",
-        ],
-        disallow: [" "],
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
   };
 }

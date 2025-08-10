ff78bc341431c1658f69aa30e27e9123a53235a6
timothyrushing <timrushing001@gmail.com>
2025-08-10 23:28:02 +0700

    chore: add fetch adapter to axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,4 +3,8 @@ import Axios from "axios";
 export const axios = Axios.create({
   baseURL: process.env.API_URL,
   params: { api_key: process.env.API_KEY },
+  adapter: "fetch",
+  fetchOptions: {
+    next: { revalidate: 3600 },
+  },
 });
